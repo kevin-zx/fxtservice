2b14e8cb1baace02b020ab8db793c9d7abd6caed
kevin-zx <zhangxian887@gmail.com>
2020-06-03 11:49:46 +0800

    初始化项目

---
+++ make.sh
@@ -0,0 +1,4 @@
+# container make
+# sudo make container GOOS=linux GOARCH=amd64
+# make
+sudo make GOOS=linux GOARCH=amd64
---
+++ pkg/config/config.go
@@ -0,0 +1,12 @@
+package config
+
+var XwDB = map[string]interface{}{
+	"ENGINE": "mysql",
+	"HOST":   "data.fxt.cn",
+	//"HOST":"localhost",
+	"PORT": 3306,
+	"NAME": "system_xiaowei", //数据库名称
+	"USER": "fxt_read",
+	//"USER":"root",
+	"PASSWORD": "jNecQlKGCwPWrOt8",
+}
